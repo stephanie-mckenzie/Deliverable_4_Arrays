907e89b85f5b23d51e83ff6b22e366c696060346
carlo <carlo@DESKTOP-I4BD20J>
2023-02-09 15:11:11 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿/* 
+ Author: Stephanie McKenzie
+ Date: February 5, 2023
+ Description: Deliverable 4 Arrays
+*/
+
+using System;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            // Delcaring variables and arrays
+            int size = 25;
+            int [] fibonacci = new int[size];
+
+            // Initialize fibonacci series         
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+            fibonacci[2] = 1;
+
+
+            for (int i = 0; i< size; i++)
+            {
+                if (i > 2)
+                {
+                    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+                }
+                Console.WriteLine("Fibonacci Number (" + i + ") = " + fibonacci[i]);
+            }
+        }
+    }
+}
